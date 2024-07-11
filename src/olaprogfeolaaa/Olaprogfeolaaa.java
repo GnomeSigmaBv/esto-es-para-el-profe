d658876f02866e7ff0ae30acbf3b3be201008c2b
Vladimir Rodríguez <jacitoshadow@gmail.com>
2024-07-11 11:57:14 -0600

    trabajo para enviar al profe

---
+++ src/olaprogfeolaaa/Olaprogfeolaaa.java
@@ -0,0 +1,18 @@
+
+package olaprogfeolaaa;
+
+public class Olaprogfeolaaa {
+
+    public static void main(String[] args) {
+        
+        
+        int numero = 10;
+        
+        
+        for (int W = 1; W <= 10; W++) {
+            int nuevo = numero * W;
+            System.out.println(nuevo);
+        }
+    }
+    
+}
